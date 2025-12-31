c8511da49b35c0f3f31f9f05742bc552445b5736
Amaan Ansari <amaanansari778@gmail.com>
2025-12-31 12:10:52 +0530

    login page

--- lib/dashboardtwo.dart
+++ lib/dashboardtwo.dart
@@ -12,9 +12,12 @@ class _DashboardtwoState extends State<Dashboardtwo> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Login app")),
+
       body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            Center(child: Text("data", textAlign: TextAlign.center)),
             SingleChildScrollView(
               scrollDirection: Axis.horizontal,
               child: Row(
@@ -22,55 +25,105 @@ class _DashboardtwoState extends State<Dashboardtwo> {
                   for (int i = 0; i < 12; i++) ...{
                     Padding(
                       padding: EdgeInsets.all(10),
-                      child: Container(
-                        decoration: BoxDecoration(
-                          image: DecorationImage(image: AssetImage("assets/mobile.jpeg"),fit: BoxFit.cover),
-                          color: Colors.blue
-                          
-                        ),
-                        width: 50,
-                        height: 80,
-                      ),
+                      child: Container(width: 50, height: 50, color: Colors.blue),
                     ),
                   },
                 ],
               ),
             ),
-
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: GridView.builder(
-                physics: NeverScrollableScrollPhysics(),
-                shrinkWrap: true,
-                itemCount: 4,
+        
+            SizedBox(height: 50),
+        
+            // Expanded(
+              // child:
+               GridView.builder(
                 gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                   crossAxisCount: 2,
                   mainAxisSpacing: 5,
-                  crossAxisSpacing: 5,
+                  crossAxisSpacing: 5
                 ),
+                itemCount: 4,
+                physics: NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
                 itemBuilder: (context, i) {
-                  return Container(
-                  decoration: BoxDecoration(image:DecorationImage(image: AssetImage("assets/acesories.jpeg"),
-                  fit: BoxFit.cover),
-                  color: Colors.blue,
-                  )
-                  ,);
+                  return Container(height: 50,width: 80,color: Colors.blue,);
                 },
               ),
-            ),
-
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Container(
-                decoration: BoxDecoration(
-                  image: DecorationImage(image: AssetImage("assets/banner.jpg"), fit: BoxFit.cover),
-                  color: Colors.red
-                ),
-                height: 200, width: double.infinity),
-            ),
+            // ),
+        
+            SizedBox(height: 50,),
+        
+            Container(
+              width: double.infinity,
+              height: 200,
+              color: Colors.yellow,
+            )
           ],
         ),
       ),
     );
+
+
+    // Scaffold(
+    //   appBar: AppBar(title: Text("Login app")),
+    //   body: SingleChildScrollView(
+    //     child: Column(
+    //       children: [
+    //         SingleChildScrollView(
+    //           scrollDirection: Axis.horizontal,
+    //           child: Row(
+    //             children: [
+    //               for (int i = 0; i < 12; i++) ...{
+    //                 Padding(
+    //                   padding: EdgeInsets.all(10),
+    //                   child: Container(
+    //                     decoration: BoxDecoration(
+    //                       image: DecorationImage(image: AssetImage("assets/mobile.jpeg"),fit: BoxFit.cover),
+    //                       color: Colors.blue
+    //                     ),
+    //                     width: 50,
+    //                     height: 80,
+    //                   ),
+    //                 ),
+    //               },
+    //             ],
+    //           ),
+    //         ),
+
+    //         Padding(
+    //           padding: const EdgeInsets.all(10.0),
+    //           child: GridView.builder(
+    //             physics: NeverScrollableScrollPhysics(),
+    //             shrinkWrap: true,
+    //             itemCount: 4,
+    //             gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+    //               crossAxisCount: 2,
+    //               mainAxisSpacing: 5,
+    //               crossAxisSpacing: 5,
+    //             ),
+    //             itemBuilder: (context, i) {
+    //               return Container(
+    //               decoration: BoxDecoration(image:DecorationImage(image: AssetImage("assets/acesories.jpeg"),
+    //               fit: BoxFit.cover),
+    //               color: Colors.blue,
+    //               )
+    //               ,);
+    //             },
+    //           ),
+    //         ),
+
+    //         Padding(
+    //           padding: const EdgeInsets.all(10.0),
+    //           child: Container(
+    //             decoration: BoxDecoration(
+    //               image: DecorationImage(image: AssetImage("assets/banner.jpg"), fit: BoxFit.cover),
+    //               color: Colors.red
+    //             ),
+    //             height: 200, width: double.infinity),
+    //         ),
+    //       ],
+    //     ),
+    //   ),
+    // );
   }
 }
