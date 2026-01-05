e2042c30ee4bc8d1123879e2cdd08bae143c4685
Amaan Ansari <amaanansari778@gmail.com>
2026-01-05 16:07:36 +0530

    login page

--- lib/model/base.model.dart
+++ lib/model/base.model.dart
@@ -1,6 +1,10 @@
+import 'package:flutter/painting.dart';
+
 class Basemodel {
   String name;
   String image;
 
-  Basemodel({required this.name, required this.image});
+  VoidCallback onPressed;
+
+  Basemodel({required this.name, required this.image, required this.onPressed});
 }
