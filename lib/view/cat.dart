4d79a62b2b1e3af5fa711c9c0977dae3cf211521
Amaan Ansari <amaanansari778@gmail.com>
2026-01-09 19:27:42 +0530

    login page

--- lib/view/cat.dart
+++ lib/view/cat.dart
@@ -20,10 +20,7 @@ class _catState extends State<cat> {
       children: [
         for(int i=0;i<widget.ofData.length;i++)...{
           Column(children: [
-            
             Container(
-
-              
               width: 150,
               height: 150,
               decoration: BoxDecoration(image: DecorationImage(image: AssetImage(widget.ofData[i].image))),),
